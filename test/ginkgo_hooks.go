47e37c9a1a8740095282a46c76e52b78d818d5d3
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-02-19 14:52:58 +0100

    fix(test): restores original $PATH

--- test/ginkgo_hooks.go
+++ test/ginkgo_hooks.go
@@ -13,10 +13,10 @@ func NewTmpPath() *TmpPath {
 	return &TmpPath{originalPath: os.Getenv("PATH")}
 }
 
-func (t *TmpPath) SetPath(paths ...string) {
+func (t *TmpPath) SetPath(paths ...string)  {
 	_ = os.Setenv("PATH", strings.Join(paths, ":"))
 }
 
 func (t *TmpPath) Restore() {
-	println(t.originalPath)
+	_ = os.Setenv("PATH", t.originalPath)
 }
