3ce574eab989c7935eb669e255c0339a2fae1f47
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-03-04 20:21:09 +0100

    chore(docs): adds circleci badge in the README (#57)
    * chore(docs): adds CircleCI badge to README
    * chore(godoc): adds comments on test server.py source code samples
    * chore(deps): cleans up Gopkg

--- e2e/test_files.go
+++ e2e/test_files.go
@@ -1,30 +1,7 @@
 package e2e
 
-const ModifiedServerPy = `
-from http.server import HTTPStatus, BaseHTTPRequestHandler
-from socketserver import TCPServer
-
-PORT = 8000
-MESSAGE = "Hello, telepresence! Ike Here!\n".encode("ascii")
-
-class Handler(BaseHTTPRequestHandler):
-    """Respond to requests with hello."""
-
-    def do_GET(self):
-        """Handle GET"""
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "text/plain")
-        self.send_header("Content-length", len(MESSAGE))
-        self.end_headers()
-        self.wfile.write(MESSAGE)
-
-
-print("Serving at port", PORT)
-TCPServer.allow_reuse_address = True
-httpd = TCPServer(("", PORT), Handler)
-httpd.serve_forever()
-`
-
+// OrigServerPy contains original server code used in `ike develop` tests
+// Based on https://github.com/datawire/hello-world
 const OrigServerPy = `
 from http.server import HTTPStatus, BaseHTTPRequestHandler
 from socketserver import TCPServer
@@ -49,4 +26,30 @@ TCPServer.allow_reuse_address = True
 httpd = TCPServer(("", PORT), Handler)
 httpd.serve_forever()
 
+`
+
+// ModifiedServerPy contains modified server response used in `ike develop` tests
+const ModifiedServerPy = `
+from http.server import HTTPStatus, BaseHTTPRequestHandler
+from socketserver import TCPServer
+
+PORT = 8000
+MESSAGE = "Hello, telepresence! Ike Here!\n".encode("ascii")
+
+class Handler(BaseHTTPRequestHandler):
+    """Respond to requests with hello."""
+
+    def do_GET(self):
+        """Handle GET"""
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", len(MESSAGE))
+        self.end_headers()
+        self.wfile.write(MESSAGE)
+
+
+print("Serving at port", PORT)
+TCPServer.allow_reuse_address = True
+httpd = TCPServer(("", PORT), Handler)
+httpd.serve_forever()
 `
