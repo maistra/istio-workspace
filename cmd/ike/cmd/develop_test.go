852256a4d656b543195ecfdd03948aed0b5458ce
Aslak Knutsen <aslak@4fs.no>
2019-03-04 15:44:25 +0100

    fix(test): enable --offline to avoid calling OC

--- cmd/ike/cmd/develop_test.go
+++ cmd/ike/cmd/develop_test.go
@@ -166,7 +166,8 @@ var _ = Describe("Usage of ike develop command", func() {
 			output, err := Run(developCmd).Passing("--deployment", "rating-service",
 				"--run", "java -jar rating.jar",
 				"--port", "4321:5000",
-				"--method", "vpn-tcp")
+				"--method", "vpn-tcp",
+				"--offline")
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(output).To(ContainSubstring("--deployment rating-service"))
@@ -177,7 +178,8 @@ var _ = Describe("Usage of ike develop command", func() {
 
 		It("should pass specified parameters and defaults", func() {
 			output, err := Run(developCmd).Passing("--deployment", "rating-service",
-				"--run", "java -jar rating.jar")
+				"--run", "java -jar rating.jar",
+				"--offline")
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(output).To(ContainSubstring("--deployment rating-service"))
@@ -201,7 +203,8 @@ var _ = Describe("Usage of ike develop command", func() {
 				"--run", "java -jar rating.jar",
 				"--build", "mvn clean install",
 				"--port", "4321",
-				"--method", "vpn-tcp")
+				"--method", "vpn-tcp",
+				"--offline")
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(output).To(ContainSubstring("mvn clean install"))
@@ -214,7 +217,8 @@ var _ = Describe("Usage of ike develop command", func() {
 				"--build", "mvn clean install",
 				"--no-build",
 				"--port", "4321",
-				"--method", "vpn-tcp")
+				"--method", "vpn-tcp",
+				"--offline")
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(output).ToNot(ContainSubstring("mvn clean install"))
