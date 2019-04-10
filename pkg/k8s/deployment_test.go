fd2b816ff4a24765821577253bb4ac0f59c24dd2
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-04-10 20:46:18 +0200

    chore(tests): implements pending deployment tests

--- pkg/k8s/deployment_test.go
+++ pkg/k8s/deployment_test.go
@@ -2,6 +2,8 @@ package k8s_test
 
 import (
 	"context"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/aslakknutsen/istio-workspace/pkg/k8s"
 	"github.com/aslakknutsen/istio-workspace/pkg/model"
@@ -16,7 +18,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Operations for k8 Deployment kind", func() {
+var _ = Describe("Operations for k8s Deployment kind", func() {
 
 	var objects []runtime.Object
 	var ctx model.SessionContext
@@ -27,6 +29,32 @@ var _ = Describe("Operations for k8 Deployment kind", func() {
 			Namespace: "test",
 			Log:       logf.Log.WithName("test"),
 			Client:    fake.NewFakeClient(objects...),
+		}
+	})
+
+	BeforeEach(func() {
+		objects = []runtime.Object{
+			&appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-ref",
+					Namespace: "test",
+					Labels: map[string]string{
+						"version": "0.0.1",
+					},
+				},
+
+				Spec: appsv1.DeploymentSpec{
+					Template: v1.PodTemplateSpec{
+						Spec: v1.PodSpec{
+							Containers: []v1.Container{
+								{
+									Image: "datawire/hello-world:latest",
+								},
+							},
+						},
+					},
+				},
+			},
 		}
 	})
 
@@ -44,31 +72,137 @@ var _ = Describe("Operations for k8 Deployment kind", func() {
 
 		It("should report false on not found", func() {
 			ref := model.Ref{Name: "test-ref-other"}
-
-			Expect(k8s.DeploymentLocator(ctx, &ref)).To(Equal(false))
+			locatorErr := k8s.DeploymentLocator(ctx, &ref)
+			Expect(locatorErr).To(BeFalse())
 		})
-		XIt("should report false on other found", func() {
-			ref := model.Ref{Name: "test-ref"}
 
-			Expect(k8s.DeploymentLocator(ctx, &ref)).To(Equal(false))
-		})
 		It("should report true on found", func() {
 			ref := model.Ref{Name: "test-ref"}
-
-			Expect(k8s.DeploymentLocator(ctx, &ref)).To(Equal(true))
+			locatorErr := k8s.DeploymentLocator(ctx, &ref)
+			Expect(locatorErr).To(BeTrue())
 		})
 
 	})
 
-	PContext("mutators", func() {
-		It("should fail invoking develop cmd when telepresence binary is not on $PATH", func() {
+	Context("mutators", func() {
+
+		BeforeEach(func() {
+			objects = []runtime.Object{
+				&appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-ref",
+						Namespace: "test",
+						Labels: map[string]string{
+							"version": "0.0.1",
+						},
+					},
+
+					Spec: appsv1.DeploymentSpec{
+						Selector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{},
+						},
+						Template: v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									{
+										Image: "datawire/hello-world:latest",
+										Env: []v1.EnvVar{},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+		})
+
+		It("should add suffix to the cloned deployment", func() {
+			ref := model.Ref{Name: "test-ref"}
+			mutatorErr := k8s.DeploymentMutator(ctx, &ref)
+			Expect(mutatorErr).ToNot(HaveOccurred())
+
+			deployment := appsv1.Deployment{}
+			err := ctx.Client.Get(ctx, types.NamespacedName{Namespace: ctx.Namespace, Name: ref.Name + "-test"}, &deployment)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		Context("telepresence mutations", func() {
+
+			It("should change container to telepresence", func() {
+				ref := model.Ref{Name: "test-ref"}
+				mutatorErr := k8s.DeploymentMutator(ctx, &ref)
+				Expect(mutatorErr).ToNot(HaveOccurred())
+
+				deployment := appsv1.Deployment{}
+				err := ctx.Client.Get(ctx, types.NamespacedName{Namespace: ctx.Namespace, Name: ref.Name + "-test"}, &deployment)
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(ContainSubstring("datawire/telepresence-k8s:0.98"))
+			})
+
+			It("should change add required env variables", func() {
+				ref := model.Ref{Name: "test-ref"}
+				mutatorErr := k8s.DeploymentMutator(ctx, &ref)
+				Expect(mutatorErr).ToNot(HaveOccurred())
+
+				deployment := appsv1.Deployment{}
+				err := ctx.Client.Get(ctx, types.NamespacedName{Namespace: ctx.Namespace, Name: ref.Name + "-test"}, &deployment)
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(deployment.Spec.Template.Spec.Containers[0].Env[0].Name).To(Equal("TELEPRESENCE_CONTAINER_NAMESPACE"))
+				Expect(deployment.Spec.Template.Spec.Containers[0].Env[0].ValueFrom).ToNot(BeNil())
+			})
+
 		})
 
 	})
 
-	PContext("revertors", func() {
+	Context("revertors", func() {
 
-		It("should fail invoking develop cmd when telepresence binary is not on $PATH", func() {
+		BeforeEach(func() {
+			objects = []runtime.Object{
+				&appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-ref",
+						Namespace: "test",
+						Labels: map[string]string{
+							"version": "0.0.1",
+						},
+					},
+
+					Spec: appsv1.DeploymentSpec{
+						Selector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{},
+						},
+						Template: v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									{
+										Image: "datawire/hello-world:latest",
+										Env: []v1.EnvVar{},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+		})
+
+		It("should revert to original deployment", func() {
+			ref := model.Ref{Name: "test-ref"}
+			mutatorErr := k8s.DeploymentMutator(ctx, &ref)
+			Expect(mutatorErr).ToNot(HaveOccurred())
+
+			revertorErr := k8s.DeploymentRevertor(ctx, &ref)
+			Expect(revertorErr).ToNot(HaveOccurred())
+
+			deployment := appsv1.Deployment{}
+			err := ctx.Client.Get(ctx, types.NamespacedName{Namespace: ctx.Namespace, Name: ref.Name }, &deployment)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(deployment.Spec.Template.Spec.Containers[0].Env).To(BeEmpty())
+			Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(ContainSubstring("datawire/hello-world:latest"))
 		})
 
 	})
