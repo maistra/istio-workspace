21972397d6ee47ee33b4c3af7b5001b2192e7c09
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-05-23 12:59:37 +0200

    chore: enhances docs of config setup

--- cmd/ike/config/config.go
+++ cmd/ike/config/config.go
@@ -12,9 +12,21 @@ import (
 	"github.com/spf13/viper"
 )
 
-// SetupConfigSources sets up Viper configuration. If specific file path is provided but fails when loading it will
-// return an error. In case of default config location it will not fail if file does not exist, but will in any other
-// case.
+// SetupConfigSources sets up Viper configuration.
+//
+// If specific file path is provided but fails when loading it will return an error.
+//
+// In case of default config location it will not fail if file does not exist,
+// but will in any other case such as parse error.
+//
+// Config precedence (each item takes precedence over the item below it):
+// . Flags
+// . Env variables
+// . Config file
+//
+// Environment variables are prefixed with `IKE` and have fully qualified names, for example
+// in case of `develop` command and its `port` flag corresponding environment variable is
+// `IKE_DEVELOP_PORT`.
 func SetupConfigSources(configFile string, notDefault bool) error {
 	viper.Reset()
 	viper.SetEnvPrefix("IKE")
@@ -35,7 +47,7 @@ func SetupConfigSources(configFile string, notDefault bool) error {
 			return err
 		}
 
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+		if _, fileDoesNotExist := err.(viper.ConfigFileNotFoundError); !fileDoesNotExist {
 			return err
 		}
 	}
