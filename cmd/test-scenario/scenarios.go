77f1c94ee30ac5277859949cacd2ea6d66bc4994
Aslak Knutsen <aslak@4fs.no>
2019-06-06 12:08:58 +0200

    Update cmd/test-scenario/scenarios.go
    Co-Authored-By: Bartosz Majsak <bartosz.majsak@gmail.com>

--- cmd/test-scenario/scenarios.go
+++ cmd/test-scenario/scenarios.go
@@ -1,7 +1,7 @@
 package main
 
 // TestScenario1 is a basic test setup with a few services calling each other in a chain. Similar to the original bookinfo example setup.
-func TestScenario1() {
+func TestScenario1_Three_Services_In_Sequence() {
 	// Scenario 1
 	services := []string{"productpage", "reviews", "ratings"}
 	Generate(
