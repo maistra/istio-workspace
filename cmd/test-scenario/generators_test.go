87f55b26a3e6595df84e9f174c69cc2295989c9a
Aslak Knutsen <aslak@4fs.no>
2019-06-11 13:38:00 +0200

    task(test): add test to ensure runtime objects are created with liveness/readiness probes. Used in tests.

--- cmd/test-scenario/generators_test.go
+++ cmd/test-scenario/generators_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	osappsv1 "github.com/openshift/api/apps/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -8,7 +12,12 @@ import (
 var _ = Describe("Operations for test scenario generator", func() {
 
 	Context("basic sub generators", func() {
-
+		validateLivenessProbe := func(template *corev1.PodTemplateSpec) {
+			Expect(template.Spec.Containers[0].LivenessProbe).ToNot(BeNil())
+		}
+		validateReadinessProbe := func(template *corev1.PodTemplateSpec) {
+			Expect(template.Spec.Containers[0].ReadinessProbe).ToNot(BeNil())
+		}
 		Context("deploymentconfig", func() {
 			It("should be created if entry is correct DeploymentType", func() {
 				obj := DeploymentConfig(Entry{"test", "DeploymentConfig"})
@@ -18,6 +27,18 @@ var _ = Describe("Operations for test scenario generator", func() {
 			It("should not be created if entry is not correct DeploymentType", func() {
 				obj := DeploymentConfig(Entry{"test", "X"})
 				Expect(obj).To(BeNil())
+			})
+
+			It("should create with liveness probe", func() {
+				obj := DeploymentConfig(Entry{"test", "DeploymentConfig"})
+				Expect(obj).To(BeAssignableToTypeOf(&osappsv1.DeploymentConfig{}))
+				validateLivenessProbe(obj.(*osappsv1.DeploymentConfig).Spec.Template)
+			})
+
+			It("should create with readiness probe", func() {
+				obj := DeploymentConfig(Entry{"test", "DeploymentConfig"})
+				Expect(obj).To(BeAssignableToTypeOf(&osappsv1.DeploymentConfig{}))
+				validateReadinessProbe(obj.(*osappsv1.DeploymentConfig).Spec.Template)
 			})
 		})
 		Context("deployment", func() {
@@ -29,6 +50,18 @@ var _ = Describe("Operations for test scenario generator", func() {
 			It("should not be created if entry is not correct DeploymentType", func() {
 				obj := Deployment(Entry{"test", "X"})
 				Expect(obj).To(BeNil())
+			})
+
+			It("should create with liveness probe", func() {
+				obj := Deployment(Entry{"test", "Deployment"})
+				Expect(obj).To(BeAssignableToTypeOf(&appsv1.Deployment{}))
+				validateLivenessProbe(&obj.(*appsv1.Deployment).Spec.Template)
+			})
+
+			It("should create with readiness probe", func() {
+				obj := Deployment(Entry{"test", "Deployment"})
+				Expect(obj).To(BeAssignableToTypeOf(&appsv1.Deployment{}))
+				validateReadinessProbe(&obj.(*appsv1.Deployment).Spec.Template)
 			})
 		})
 	})
