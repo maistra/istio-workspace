458555e5944e05d6cefc391e49c865651137dca1
Aslak Knutsen <aslak@4fs.no>
2019-06-11 09:22:08 +0200

    Update pkg/openshift/openshift_suite_test.go
    Co-Authored-By: Bartosz Majsak <bartosz.majsak@gmail.com>

--- pkg/openshift/openshift_suite_test.go
+++ pkg/openshift/openshift_suite_test.go
@@ -15,3 +15,11 @@ func TestOpenshift(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecWithJUnitReporter(t, "Openshift object Suite")
 }
+
+var _ = SynchronizedAfterSuite(func() {}, func() {
+	goleak.VerifyNone(GinkgoT(),
+		goleak.IgnoreTopFunction("github.com/maistra/istio-workspace/vendor/k8s.io/klog.(*loggingT).flushDaemon"),
+		goleak.IgnoreTopFunction("github.com/maistra/istio-workspace/vendor/github.com/onsi/ginkgo/internal/specrunner.(*SpecRunner).registerForInterrupts"),
+	)
+})
+
