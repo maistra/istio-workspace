47a99b99f5293f305d7c589b41ff869963a500d3
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-06-13 13:16:34 +0200

    feat: collect tpl parameters

--- cmd/ike/config/env_vars.go
+++ cmd/ike/config/env_vars.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"github.com/maistra/istio-workspace/pkg/openshift/parser"
+
+	openshiftApi "github.com/openshift/api/template/v1"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
 // These consts are used across the project to load corresponding env variables
 // Extend when needed
 const (
@@ -7,3 +14,25 @@ const (
 	EnvDockerRegistry   = "IKE_DOCKER_REGISTRY"
 )
 
+var (
+	log        = logf.Log.WithName("session_handler")
+	parameters []openshiftApi.Parameter
+)
+
+func init() {
+	collectTplParams("operator.yaml")
+	collectTplParams("role_binding.yaml")
+}
+
+func collectTplParams(resource string) {
+	tpl, err := parser.Load(resource)
+	if err != nil {
+		log.Error(err, "failed parsing "+resource+"template")
+	}
+
+	tplParams, err := parser.ParseParameters(tpl)
+	if err != nil {
+		log.Error(err, "failed parsing parameters in the template "+resource)
+	}
+	parameters = append(parameters, tplParams...)
+}
