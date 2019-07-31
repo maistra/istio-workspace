c16447977ee79ebf6c75143a59ec56d13866f36e
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-07-31 14:02:33 +0200

    chore: dev script for deploying operator

---
+++ scripts/dev/deploy-operator.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export IKE_DOCKER_REGISTRY=docker-registry-default.127.0.0.1.nip.io:80
+export IKE_DOCKER_REPOSITORY=istio-workspace-operator
+export TELEPRESENCE_VERSION=$(telepresence --version)
+export IKE_IMAGE_TAG="$(make version)"
+
+oc new-project istio-workspace-operator || true
+oc adm policy add-scc-to-user anyuid -z default -n $(oc project -q)
+oc adm policy add-scc-to-user privileged -z default -n $(oc project -q)
+
+docker login -u $(oc whoami) -p $(oc whoami -t) $(echo $IKE_DOCKER_REGISTRY)
+make docker-build docker-push
+export IKE_DOCKER_REGISTRY=172.30.1.1:5000
+ike install-operator
