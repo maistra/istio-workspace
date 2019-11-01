c531d72d26e80836c5a9095d7523839bd1f28758
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-11-01 10:39:06 +0100

    chore: removes hardcoded tp version

--- pkg/telepresence/wrapper.go
+++ pkg/telepresence/wrapper.go
@@ -1,6 +1,7 @@
 package telepresence
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -10,15 +11,15 @@ import (
 )
 
 const (
-	BinaryName                 = "telepresence"
-	DefaultTelepresenceVersion = "0.101"
+	BinaryName  = "telepresence"
+	installHint = "Head over to https://www.telepresence.io/reference/install for installation instructions.\n"
 )
 
 // GetVersion checks which version of telepresence should be used or is available on the path
 // TELEPRESENCE_VERSION env variable is checked and if is defined takes precedence.
 // If the binary is present on the $PATH then its version is used.
-// If all above fails we return telepresence.DefaultTelepresenceVersion
-func GetVersion() string {
+// If all above fails we return error, as there's no telepresence in use nor env var is defined
+func GetVersion() (string, error) {
 	tpVersion, found := os.LookupEnv("TELEPRESENCE_VERSION")
 	if !found && BinaryAvailable() {
 		done := make(chan gocmd.Status, 1)
@@ -35,12 +36,14 @@ func GetVersion() string {
 	}
 
 	if tpVersion == "" {
-		tpVersion = DefaultTelepresenceVersion
+		return "", fmt.Errorf("couldn't find '%s' installed in your system.\n"+
+			"%s\n"+
+			"you can specify the version using TELEPRESENCE_VERSION environment variable", BinaryName, installHint)
 	}
-	return tpVersion
+	return tpVersion, nil
 }
 
 // BinaryAvailable checks if telepresence binary is available on the path
 func BinaryAvailable() bool {
-	return shell.BinaryExists(BinaryName, "Head over to https://www.telepresence.io/reference/install for installation instructions.\n")
+	return shell.BinaryExists(BinaryName, installHint)
 }
