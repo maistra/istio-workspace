8797c4706e2bf15db3c91dcba73a4c64f572689f
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-12-12 19:54:04 +0100

    release notes!

--- scripts/release.sh
+++ scripts/release.sh
@@ -56,13 +56,33 @@ while test $# -gt 0; do
   esac
 done
 
-./validate.sh ${version}
+/bin/bash "${BASEDIR}"/scripts/validate.sh "${version}"
+
+if [[ $? -ne 0 ]]; then
+  die "Please correct errors!"
+fi
+
+## Check if tag exists
+tag_exists=$(git --no-pager tag --list | grep -c ${version})
+if [[ ${tag_exists} -ne 0 ]]; then
+  die "Tag ${version} already exists!"
+fi
+
+## Ensure you are on master
+current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+if [[ ${current_branch} != "master" ]]; then
+  die "You are on ${current_branch} branch. Switch to master!"
+fi
+
+## Generate changelog and append it to the highlights
+ghc=$(curl -sL http://git.io/install-ghc | bash -s -- --path-only)
+${ghc}/ghc generate -r maistra/istio-workspace --format adoc >> docs/modules/ROOT/pages/release_notes/${version}.adoc
 
 ## Replace antora version for docs build
 sed -i "/version:/c\version: ${version}" docs/antora.yml
 sed -i "/^== Releases.*/a include::release_notes\/${version}.adoc[]\n" docs/modules/ROOT/pages/release_notes.adoc
 
-git commit -am"release: ${version}"
+git add . && git commit -am"release: ${version}"
 git tag -a "${version}" -m"Automatically created release tag"
 
 ## Prepare next release iteration
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -14,11 +14,6 @@ validate_version() {
     die "Undefined version (pass using -v|--version). Please use semantic version. Read more about it here: https://semver.org/ \n\n"
   fi
 
-  tag_exists=$(git --no-pager tag --list | grep -c ${version})
-  if [[ ${tag_exists} -ne 0 ]]; then
-    die "Tag ${version} already exists!"
-  fi
-
   # ensure defined version matches semver rules
   if [[ ! "${version}" =~ $sem_ver_pattern ]]; then
     die "Version (${version}) you defined does not match semantic version. Read more about it here: https://semver.org/"
@@ -27,12 +22,6 @@ validate_version() {
   # ensure release notes exist
   if [[ ! -f "docs/modules/ROOT/pages/release_notes/${version}.adoc" ]]; then
     die "Please create release notes in docs/modules/ROOT/pages/release_notes/${version}.adoc and submit it over a Pull Request."
-  fi
-
-  # ensure you are on master
-  current_branch=$(git branch | grep \* | cut -d ' ' -f2)
-  if [[ ${current_branch} != "master" ]]; then
-    die "You are on ${current_branch} branch. Switch to master!"
   fi
 }
 
