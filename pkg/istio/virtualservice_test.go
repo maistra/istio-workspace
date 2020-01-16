49f624a61bc4e2e0b5661214ba40ce3c63602a4d
Aslak Knutsen <aslak@4fs.no>
2020-01-16 08:10:24 +0100

    fix(operator): revert all manipulated virtual service routes
    fixes #350

--- pkg/istio/virtualservice_test.go
+++ pkg/istio/virtualservice_test.go
@@ -195,7 +195,7 @@ var _ = Describe("Operations for istio VirtualService kind", func() {
 				)
 
 				BeforeEach(func() {
-					yaml = simpleMutatedVirtualService
+					yaml = complextMutatedVirtualService
 					ctx = model.SessionContext{
 						Route: model.Route{
 							Type:  "header",
@@ -211,7 +211,7 @@ var _ = Describe("Operations for istio VirtualService kind", func() {
 				})
 
 				It("route removed", func() {
-					Expect(revertedVirtualService.Spec.Http).To(HaveLen(1))
+					Expect(revertedVirtualService.Spec.Http).To(HaveLen(2))
 				})
 
 				It("correct route removed", func() {
@@ -315,29 +315,49 @@ spec:
         host: x
 `
 
-var simpleMutatedVirtualService = `kind: VirtualService
+var complextMutatedVirtualService = `apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
 metadata:
-  creationTimestamp: "2019-01-16T20:58:51Z"
-  generation: 1
-  name: details
-  namespace: bookinfo
-  resourceVersion: "4978223"
-  selfLink: /apis/networking.istio.io/v1alpha3/namespaces/bookinfo/virtualservices/details
-  uid: 86e9c879-19d1-11e9-a489-482ae3045b54
+  creationTimestamp: "2020-01-15T22:53:54Z"
+  generation: 11
+  name: reviews
+  namespace: aslak-devconf
+  resourceVersion: "591982"
+  selfLink: /apis/networking.istio.io/v1alpha3/namespaces/aslak-devconf/virtualservices/reviews
+  uid: e7a2a377-37e9-11ea-bd3f-02fb5d7d8a95
 spec:
   hosts:
   - details
   http:
   - match:
     - headers:
-        end-user:
-          exact: jason
+        x-test-suite:
+          exact: feature-x
     route:
     - destination:
         host: details
         subset: v1-vs-test
+  - match:
+    - headers:
+        x-test-suite:
+          exact: feature-x
+      uri:
+        prefix: /test-service
+    rewrite:
+      uri: /
+    route:
+    - destination:
+        host: details
+        subset: v1-vs-test
+  - match:
+    - uri:
+        prefix: /test-service
+    rewrite:
+      uri: /
+    route:
+    - destination:
+        host: details
   - route:
     - destination:
         host: details
-        subset: v1
-`
+        subset: v1`
