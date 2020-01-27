7011dfc4e248cb50fc46a6c6f3a9d4cb50eca7e6
Bartosz Majsak <bartosz.majsak@gmail.com>
2020-01-27 14:14:17 +0100

    chore: update e2e/infra/test_service.go
    Co-Authored-By: Bot from GolangCI <42910462+golangcibot@users.noreply.github.com>

--- e2e/infra/test_service.go
+++ e2e/infra/test_service.go
@@ -39,7 +39,7 @@ func BuildTestServicePreparedImage(callerName, namespace string) (registry strin
 	os.Setenv("IKE_TEST_PREPARED_NAME", callerName)
 
 	LoginAsTestPowerUser()
-	<-shell.ExecuteInDir(".", "bash", "-c", "docker login -u " + user + " -p $(oc whoami -t) "+registry).Done()
+	<-shell.ExecuteInDir(".", "bash", "-c", "docker login -u "+user+" -p $(oc whoami -t) "+registry).Done()
 	<-shell.ExecuteInDir(projectDir, "make", "docker-build-test-prepared", "docker-push-test-prepared").Done()
 	return
 }
