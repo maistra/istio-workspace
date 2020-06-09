85d7bb1c908f52bb9dee5870150f437b7ad6b1bb
Aslak Knutsen <aslak@4fs.no>
2020-06-09 14:48:41 +0200

    fix(e2e): ensure route is accessible via host and header

--- e2e/infra/test_service.go
+++ e2e/infra/test_service.go
@@ -2,6 +2,7 @@ package infra
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 
@@ -75,6 +76,25 @@ func setTestNamespace(namespace string) {
 // GetGatewayHost returns the host the Gateway in the scenario is bound to (http header Host)
 func GetGatewayHost(namespace string) string {
 	return namespace + "-test.com"
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyz"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+// StringWithCharset returns a random string of length based on charset
+func stringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+// GenerateSessionName returns a random safe string to be used as a session name
+func GenerateSessionName(length int) string {
+	return stringWithCharset(length, charset)
 }
 
 // PublisherRuby contains fixed response to be changed by tests
