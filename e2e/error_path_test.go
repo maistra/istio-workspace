00cb1f5d1b2a522f6891caa875f35e78ffa39691
Aslak Knutsen <aslak@4fs.no>
2021-01-18 17:18:48 +0100

    bah

--- e2e/error_path_test.go
+++ e2e/error_path_test.go
@@ -40,7 +40,7 @@ var _ = Describe("Smoke End To End Tests - Faulty scenarios", func() {
 
 			PrepareEnv(namespace)
 			InstallLocalOperator(namespace)
-			Eventually(AllDeploymentsAndPodsReady(namespace), 2*time.Minute, 5*time.Second).Should(BeTrue())
+			Eventually(AllDeploymentsAndPodsReady(namespace), 10*time.Minute, 5*time.Second).Should(BeTrue())
 		})
 
 		AfterEach(func() {
