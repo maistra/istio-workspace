3f9d94c9af5a84bb6659e7674fe7977140b33011
Aslak Knutsen <aslak@4fs.no>
2021-02-09 15:10:08 +0100

    fix: do pr

--- scripts/release/operatorhub.sh
+++ scripts/release/operatorhub.sh
@@ -2,11 +2,12 @@
 
 set -e
 
+GITHUB_USER="${GITHUB_USER:-alien-ike}"
 CURR_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OWNER="${OWNER:-operator-framework}"
 HUB_REPO_URL="${HUB_REPO_URL:-https://github.com/${OWNER}/community-operators.git}"
 FORK="${FORK:-maistra}"
-FORK_REPO_URL="${FORK_REPO_URL:-https://github.com/${FORK}/community-operators.git}"
+FORK_REPO_URL="${FORK_REPO_URL:-https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${FORK}/community-operators.git}"
 TEMP_FOLDER=$(mktemp -d)
 OPERATOR_NAME=istio-workspace-operator
 OPERATOR_VERSION=${OPERATOR_VERSION:-0.0.5} # TODO replace during release process?
@@ -22,7 +23,9 @@ if [[ -z $GITHUB_TOKEN ]]; then
 fi
 
 # Validate version
-./"${CURR_FOLDER}"/validate.sh "v${OPERATOR_VERSION}" --skip-ensure-release-notes
+# TODO: ./path doesn't work?
+# TODO: validate failes due to existing v. Expected to be called before release, not after
+#sh "${CURR_FOLDER}"/validate.sh "v${OPERATOR_VERSION}" --skip-ensure-release-notes
 
 git clone "${HUB_REPO_URL}" "${TEMP_FOLDER}"
 
@@ -33,22 +36,28 @@ git remote add fork "${FORK_REPO_URL}"
 git checkout -b "${BRANCH_NAME}"
 
 mkdir -p community-operators/"${OPERATOR_NAME}"/"${OPERATOR_VERSION}"/
-cp -R "${CURR_FOLDER}"/bundle/ community-operators/"${OPERATOR_NAME}"/"${OPERATOR_VERSION}"/
+cp -R "${CURR_FOLDER}"/../../bundle/ community-operators/"${OPERATOR_NAME}"/"${OPERATOR_VERSION}"/
 
 # commit - signed
 git add .
-git commit -s -m"adds ${OPERATOR_NAME} release ${OPERATOR_VERSION}"
+git commit -S -m"add ${OPERATOR_NAME} release ${OPERATOR_VERSION}"
 
 git push fork "${BRANCH_NAME}"
+
+
+TEMP_PAYLOAD=$(mktemp)
+
+jq -c -n \
+  --arg msg "$(cat ${CURR_FOLDER}/operatorhub-pr-template.md)" \
+  --arg head ${FORK}:${BRANCH_NAME} \
+  --arg title "add ${OPERATOR_NAME} release ${OPERATOR_VERSION}" \
+   '{head: $head, base: "master", title: $title, body: $msg }' > ${TEMP_PAYLOAD}
 
 curl \
   -X POST \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/"${OWNER}"/community-operators/pulls \
-  -d "{\"head\":\"${FORK}:${BRANCH_NAME}\",\"base\":\"master\"}"
-  ## TODO add body
-
-## Test fork -> maistra PR
+   --data-binary "@${TEMP_PAYLOAD}"
 
 
