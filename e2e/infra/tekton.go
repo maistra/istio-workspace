031c53c7af79d5f65b5c878619ae19ed24beae59
Bartosz Majsak <bartosz.majsak@gmail.com>
2021-03-11 13:36:44 +0100

    fix: unclosed action

--- e2e/infra/tekton.go
+++ e2e/infra/tekton.go
@@ -9,7 +9,7 @@ import (
 // TaskIsDone checks if given task has succeeded.
 func TaskIsDone(ns, taskName string) func() bool {
 	return func() bool {
-		taskRunStatus := shell.ExecuteInDir(".", "kubectl", "get", "taskruns", taskName, "-n", ns, "-o", "jsonpath='{.status.conditions[?(.type==\"Succeeded\")].reason")
+		taskRunStatus := shell.ExecuteInDir(".", "kubectl", "get", "taskruns", taskName, "-n", ns, "-o", "jsonpath='{.status.conditions[?(.type==\"Succeeded\")].reason}'")
 		<-taskRunStatus.Done()
 		return strings.Contains(strings.Join(taskRunStatus.Status().Stdout, ""), "Succeeded")
 	}
@@ -17,7 +17,7 @@ func TaskIsDone(ns, taskName string) func() bool {
 
 // TaskResult returns value of given result variable for defined Task.
 func TaskResult(ns, taskName, key string) string {
-	taskResultStatus := shell.ExecuteInDir(".", "kubectl", "get", "taskruns", taskName, "-n", ns, "-o", "jsonpath='{.status.taskResults[?(.name==\""+key+"\")].value")
+	taskResultStatus := shell.ExecuteInDir(".", "kubectl", "get", "taskruns", taskName, "-n", ns, "-o", "jsonpath='{.status.taskResults[?(.name==\""+key+"\")].value}'")
 	<-taskResultStatus.Done()
 	return strings.Join(taskResultStatus.Status().Stdout, "")
 }
