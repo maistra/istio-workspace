95f9de2aa079a1e4fcdab94f26ec495d83cbbc98
Bartosz Majsak <bartosz.majsak@gmail.com>
2021-05-04 15:23:15 +0200

    fix(session): makes sure session name follows RFC 1123 (#835)

--- pkg/internal/session/session_test.go
+++ pkg/internal/session/session_test.go
@@ -45,7 +45,7 @@ var _ = Describe("Session operations", func() {
 		})
 		Context("create", func() {
 
-			It("should create a new session if non found", func() {
+			It("should create a new session if none found", func() {
 				// given - no exiting sessions
 				// when - adding a ref to a session
 				_, remove, err := session.CreateOrJoinHandler(opts, client)
