06fc70bdf637dfa5b7723a26d7a2393b21adc528
Bartosz Majsak <bartosz.majsak@gmail.com>
2021-05-26 13:01:03 +0200

    round #4

--- pkg/k8s/deployment_test.go
+++ pkg/k8s/deployment_test.go
@@ -148,7 +148,7 @@ var _ = Describe("Operations for k8s Deployment kind", func() {
 
 		CreateTestLocatorStore2 := func(kind string) new.LocatorStore {
 			l := new.LocatorStore{}
-			l.Report(new.LocatorStatus{Kind: kind, Name: "test-ref", Labels: map[string]string{"version": "v1"}, Action: new.ActionLocated})
+			l.Report(new.LocatorStatus{Kind: kind, Name: "test-ref", Namespace: "test", Labels: map[string]string{"version": "v1"}, Action: new.ActionCreate})
 			return l
 		}
 
