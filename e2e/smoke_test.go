8c587fa93883c9b212dfb1797bf0f52f5e0aa12e
Bartosz Majsak <bartosz.majsak@gmail.com>
2021-07-12 13:01:42 +0200

    tmp: exclude more create/delete tests

--- e2e/smoke_test.go
+++ e2e/smoke_test.go
@@ -317,7 +317,7 @@ var _ = Describe("Smoke End To End Tests", func() {
 
 		})
 
-		Context("verify external integrations", func() {
+		XContext("verify external integrations", func() {
 
 			Context("Tekton", func() {
 
