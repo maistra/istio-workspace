5a024564653b125a09258382e6bb2818a2e29cae
Bartosz Majsak <bartosz.majsak@gmail.com>
2021-07-22 11:52:23 +0200

    fix: brings back rootDir

--- test/cmd/test-service/main.go
+++ test/cmd/test-service/main.go
@@ -29,6 +29,8 @@ const (
 	EnvServiceCall = "SERVICE_CALL"
 )
 
+var rootDir = "test/cmd/test-service/assets/" //nolint:varcheck,deadcode,unused //reason This is required to use the dev mode for assets (reading from fs)
+
 // Config describes the basic Name and who to call next for a given HandlerFunc.
 type Config struct {
 	Name string
