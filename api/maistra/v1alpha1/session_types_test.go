ecfa131c39f898fb898c5782eae312a1f080bc75
Aslak Knutsen <aslak@4fs.no>
2021-09-09 16:19:54 +0200

    fix typo

--- api/maistra/v1alpha1/session_types_test.go
+++ api/maistra/v1alpha1/session_types_test.go
@@ -12,60 +12,60 @@ var _ = Describe("API manipulation", func() {
 	Context("when flipping readiness", func() {
 
 		var components v1alpha1.StatusComponents
-		compoentOne := "one"
-		compoentTwo := "two"
+		componentOne := "one"
+		componentTwo := "two"
 
 		BeforeEach(func() {
 			components = v1alpha1.StatusComponents{}
 		})
 
 		It("should set as pending", func() {
-			components.SetPending(compoentOne)
+			components.SetPending(componentOne)
 
 			Expect(components.Pending).To(HaveLen(1))
 		})
 		It("should set as Ready", func() {
-			components.SetReady(compoentOne)
+			components.SetReady(componentOne)
 
 			Expect(components.Ready).To(HaveLen(1))
 		})
 		It("should set as UnReady", func() {
-			components.SetUnReady(compoentOne)
+			components.SetUnReady(componentOne)
 
 			Expect(components.UnReady).To(HaveLen(1))
 		})
 
 		It("should flip from pending to ready", func() {
-			components.SetPending(compoentOne)
+			components.SetPending(componentOne)
 			Expect(components.Pending).To(HaveLen(1))
 
-			components.SetReady(compoentOne)
+			components.SetReady(componentOne)
 			Expect(components.Ready).To(HaveLen(1))
 			Expect(components.Pending).To(HaveLen(0))
 		})
 		It("should flip from pending to unready", func() {
-			components.SetPending(compoentOne)
+			components.SetPending(componentOne)
 			Expect(components.Pending).To(HaveLen(1))
 
-			components.SetUnReady(compoentOne)
+			components.SetUnReady(componentOne)
 			Expect(components.UnReady).To(HaveLen(1))
 			Expect(components.Pending).To(HaveLen(0))
 		})
 		It("should flip from ready to unready", func() {
-			components.SetReady(compoentOne)
+			components.SetReady(componentOne)
 			Expect(components.Ready).To(HaveLen(1))
 
-			components.SetUnReady(compoentOne)
+			components.SetUnReady(componentOne)
 			Expect(components.UnReady).To(HaveLen(1))
 			Expect(components.Ready).To(HaveLen(0))
 		})
 
 		It("should only flip one object", func() {
-			components.SetReady(compoentOne)
-			components.SetReady(compoentTwo)
+			components.SetReady(componentOne)
+			components.SetReady(componentTwo)
 			Expect(components.Ready).To(HaveLen(2))
 
-			components.SetUnReady(compoentOne)
+			components.SetUnReady(componentOne)
 			Expect(components.UnReady).To(HaveLen(1))
 			Expect(components.Ready).To(HaveLen(1))
 		})
