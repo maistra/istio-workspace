416cadf51e2152389cc28a5f51e0bdf4426882e5
Bartosz Majsak <bartosz.majsak@gmail.com>
2021-10-15 17:44:26 +0200

    chore: extracts task preparation logic to its own script
    so it can be reused while publishing to tekton hub or deploying to cluster

---
+++ scripts/release/tektonhub/prepare_task.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function replace_placeholders() {
+  local path=$1
+  local taskVersion=$2
+  local image=$3
+  local cmd=( "sed" )
+
+  if [ -n "${4:-}" ]; then
+      cmd+=( -i )
+  fi
+  cmd+=( -e "s/released-image/${image}/g" -e "s/current-version/${taskVersion}/g" "${path}")
+  "${cmd[@]}"
+}
+
+case ${1-noop} in
+    replace_placeholders) "$@"; exit;;
+esac
