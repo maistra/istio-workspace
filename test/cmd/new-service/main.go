7cc3db950d7c9f345bcfba12db73ff96b80cf98a
bartoszmajsak <bartosz.majsak@gmail.com>
2022-07-15 10:16:53 +0200

    chore: adds error handling to new-service

--- test/cmd/new-service/main.go
+++ test/cmd/new-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"io/ioutil"
 	"log"
@@ -10,7 +11,6 @@ import (
 // Simple web server to verify if we can reach services in the cluster when running it locally.
 // Used for "ike develop new" scenario.
 func main() {
-
 	port := flag.String("port", ":8181", "The address this service is available on")
 	target := flag.String("target", "http://reviews:9080", "The target service to be called")
 	flag.Parse()
@@ -18,10 +18,17 @@ func main() {
 	log.Println("Starting new service on", *port)
 
 	if err := http.ListenAndServe(*port, http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		resp, err := http.Get(*target)
+		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, *target, nil)
 		if err != nil {
-			writer.WriteHeader(http.StatusInternalServerError)
-			writer.Write([]byte(err.Error()))
+			respondWithErr(writer, err)
+
+			return
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			respondWithErr(writer, err)
+
 			return
 		}
 
@@ -29,8 +36,13 @@ func main() {
 		body := resp.Body
 		defer body.Close()
 		content, _ := ioutil.ReadAll(body)
-		writer.Write(content)
+		_, _ = writer.Write(content)
 	})); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+}
+
+func respondWithErr(writer http.ResponseWriter, e error) {
+	writer.WriteHeader(http.StatusInternalServerError)
+	_, _ = writer.Write([]byte(e.Error()))
 }
