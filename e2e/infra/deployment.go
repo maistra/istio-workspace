5e62d9c66490b040dbc5bdbdb33833b4b4e86a42
bartoszmajsak <bartosz.majsak@gmail.com>
2022-07-20 19:48:12 +0200

    chore: removes gomega dependency from non-test deployment.go

--- e2e/infra/deployment.go
+++ e2e/infra/deployment.go
@@ -3,7 +3,6 @@ package infra
 import (
 	"os"
 
-	"github.com/onsi/gomega"
 	"github.com/spf13/afero"
 )
 
@@ -12,19 +11,31 @@ var appFs = afero.NewOsFs()
 // CreateFile creates file under defined path with a given content.
 func CreateFile(filePath, content string) {
 	file, err := appFs.Create(filePath)
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-	err = appFs.Chmod(filePath, os.ModePerm)
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-	_, err = file.WriteString(content)
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-	err = file.Close()
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+
+	defer func() {
+		if err = file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err = appFs.Chmod(filePath, os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	if _, err = file.WriteString(content); err != nil {
+		panic(err)
+	}
 }
 
 // DeleteFile deletes file under defined path.
 func DeleteFile(filePath string) {
-	err := appFs.Remove(filePath)
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	if err := appFs.Remove(filePath); err != nil {
+		panic(err)
+	}
 }
 
 func NewProjectCmd(name string) string {
