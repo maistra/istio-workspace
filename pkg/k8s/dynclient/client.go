8b1d777924638c6b1c0363f729d3d9df6a721663
bartoszmajsak <bartosz.majsak@gmail.com>
2022-07-22 08:01:28 +0200

    feat(k8s): brings back dynamic client
    This will allow us to easily create required objects for new service use
    case without a need for type-safe counterparts.

---
+++ pkg/k8s/dynclient/client.go
@@ -0,0 +1,123 @@
+package dynclient
+
+import (
+	"context"
+
+	errors2 "emperror.dev/errors"
+	coreV1 "k8s.io/api/core/v1"
+	rbacV1 "k8s.io/api/rbac/v1"
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type Client struct {
+	Namespace string
+	dynClient dynamic.Interface
+	clientset *kubernetes.Clientset
+	mapper    meta.RESTMapper
+}
+
+// NewDefaultDynamicClient creates dynamic client for given ns.
+func NewDefaultDynamicClient(namespace string, createNs bool) (*Client, error) {
+	kubeCfg := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	)
+
+	restCfg, err := kubeCfg.ClientConfig()
+	if err != nil {
+		return nil, errors2.Wrap(err, "failed configuring k8s client")
+	}
+
+	clientset, err := kubernetes.NewForConfig(restCfg)
+	if err != nil {
+		return nil, errors2.Wrap(err, "failed creating k8s clientset")
+	}
+
+	dynClient, err := dynamic.NewForConfig(restCfg)
+	if err != nil {
+		return nil, errors2.Wrap(err, "failed creating k8s dynamic client")
+	}
+
+	groupResources, err := restmapper.GetAPIGroupResources(clientset.Discovery())
+	if err != nil {
+		return nil, errors2.Wrap(err, "failed obtaining APIGroupResources")
+	}
+
+	rm := restmapper.NewDiscoveryRESTMapper(groupResources)
+
+	client := Client{dynClient: dynClient,
+		clientset: clientset,
+		mapper:    rm,
+		Namespace: namespace}
+
+	if createNs {
+		err = client.createNamespaceIfNotExists()
+	}
+
+	return &client, err
+}
+
+func (c *Client) Create(obj runtime.Object) error {
+	err := c.createNamespaceIfNotExists()
+	if err != nil {
+		return err
+	}
+
+	var resourceInterface dynamic.ResourceInterface
+	nsResourceInterface, err := c.resourceInterfaceFor(obj)
+	if err != nil {
+		return errors2.Wrap(err, "failed obtaining resource interface")
+	}
+
+	resourceInterface = nsResourceInterface
+
+	switch obj.(type) {
+	case *v1.CustomResourceDefinition:
+	case *rbacV1.ClusterRole:
+	case *rbacV1.ClusterRoleBinding:
+	default:
+		// For all the other types we should create resources in the desired namespace
+		resourceInterface = nsResourceInterface.Namespace(c.Namespace)
+	}
+
+	unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return errors2.Wrap(err, "failed converting object to unstructed object")
+	}
+
+	_, err = resourceInterface.Create(context.Background(), &unstructured.Unstructured{Object: unstructuredObj}, metav1.CreateOptions{})
+
+	return errors2.Wrap(err, "failed creating object")
+}
+
+func (c *Client) createNamespaceIfNotExists() error {
+	nsSpec := &coreV1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: c.Namespace}}
+	_, err := c.clientset.CoreV1().Namespaces().Create(context.Background(), nsSpec, metav1.CreateOptions{})
+	if errors.IsAlreadyExists(err) {
+		return nil
+	}
+
+	return errors2.Wrap(err, "failed creating new namespace")
+}
+
+func (c *Client) resourceInterfaceFor(raw runtime.Object) (dynamic.NamespaceableResourceInterface, error) {
+	gvk := raw.GetObjectKind().GroupVersionKind()
+	gk := schema.GroupKind{Group: gvk.Group, Kind: gvk.Kind}
+	mapping, err := c.mapper.RESTMapping(gk, gvk.Version)
+	if err != nil {
+		return nil, errors2.Wrap(err, "failed mapping runtime object")
+	}
+	resource := c.dynClient.Resource(mapping.Resource)
+
+	return resource, nil
+}
