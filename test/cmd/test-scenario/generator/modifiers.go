5867e0b43d484c4f3ecc200d374682bdc5cb2549
bartoszmajsak <bartosz.majsak@gmail.com>
2022-11-08 16:37:21 +0100

    fix(scenario): prepends gw with its ns in the virtualservice

--- test/cmd/test-scenario/generator/modifiers.go
+++ test/cmd/test-scenario/generator/modifiers.go
@@ -11,10 +11,15 @@ import (
 
 // ConnectToGateway modifier to connect VirtualService to a Gateway. Combine with ForService.
 func ConnectToGateway(hostname string) Modifier {
+	gatewayNs := GatewayNamespace
+	if gatewayNs == "" {
+		gatewayNs = Namespace
+	}
+	
 	return func(service Entry, object runtime.Object) {
 		if obj, ok := object.(*istionetwork.VirtualService); ok {
 			obj.Spec.Hosts = []string{hostname}
-			obj.Spec.Gateways = append(obj.Spec.Gateways, "test-gateway")
+			obj.Spec.Gateways = append(obj.Spec.Gateways, gatewayNs+"/test-gateway")
 			for i := 0; i < len(obj.Spec.Http); i++ {
 				http := obj.Spec.Http[i]
 				for n := 0; n < len(http.Route); n++ {
